66f6270f06ebfc5f47a9f7642b2c1af0d5aff3c0
Teusner <quentin.brateau@ensta-bretagne.org>
2023-02-23 09:43:38 +0100

    Update

--- scripts/riptide_wtf.py
+++ scripts/riptide_wtf.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import rclpy
+from rclpy.time import Time
 from rclpy.node import Node
 from riptide_msgs.msg import Pressure, Actuators
 from sensor_msgs.msg import Imu, BatteryState
@@ -25,6 +26,11 @@ class RiptideWTF(Node):
         self.actuators_msg = Actuators()
         self.imu_msg = Imu()
 
+        self.pressure_time = self.get_clock().now()
+        self.battery_card_time = self.get_clock().now()
+        self.actuators_time = self.get_clock().now()
+        self.imu_time = self.get_clock().now()
+
         self.init_ncurses()
 
         timer_period = 1  # seconds
@@ -48,6 +54,8 @@ class RiptideWTF(Node):
         curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
         curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
         curses.init_pair(3, curses.COLOR_BLUE, curses.COLOR_BLACK)
+        curses.init_color(8, 498, 549, 553)
+        curses.init_pair(4, 8, curses.COLOR_BLACK)
 
         self.daemonWindow = curses.newwin(4, 65, 1, 1)
         self.daemonWindow.box()
@@ -94,8 +102,21 @@ class RiptideWTF(Node):
 
         self.daemonWindow.refresh()
 
+    def duration_ms_from_times(self, t0):
+        t = self.get_clock().now() - t0
+        return int(t.nanoseconds / 1e6)
+
     def pressure_window(self):
         self.pressureWindow.addstr(1, 2, "• Pressure", curses.A_BOLD | curses.color_pair(3))
+
+        # Timestamp
+        duration = self.duration_ms_from_times(self.pressure_time)
+        if (duration < 100):
+            color = curses.color_pair(1)
+        else:
+            color = curses.color_pair(2)
+        self.pressureWindow.addstr(1, 18, f"{duration} ms".rjust(10), color)
+
         self.pressureWindow.addstr(2, 4, "pressure:")
         self.pressureWindow.addstr(2, 20, f"{self.pressure_msg.pressure:.2f} mbar".rjust(10))
         self.pressureWindow.addstr(3, 4, "temperature:")
@@ -109,25 +130,52 @@ class RiptideWTF(Node):
     def actuators_window(self):
         self.actuatorsWindow.addstr(1, 2, "• Actuators", curses.A_BOLD | curses.color_pair(3))
         self.actuatorsWindow.addstr(2, 4, "thruster:")
-        self.actuatorsWindow.addstr(2, 20, f"{self.actuators_msg.thruster:.2f} mbar".rjust(10))
+
+        # Timestamp
+        duration = self.duration_ms_from_times(self.actuators_time)
+        if (duration < 100):
+            color = curses.color_pair(1)
+        else:
+            color = curses.color_pair(2)
+        self.actuatorsWindow.addstr(1, 19, f"{duration} ms".rjust(10), color)
+
+        self.actuatorsWindow.addstr(2, 20, f"{self.actuators_msg.thruster:.2f} usi".rjust(10))
         self.actuatorsWindow.addstr(3, 4, "d_fin:")
-        self.actuatorsWindow.addstr(3, 18, f"{self.actuators_msg.d_fin:.2f} °C".rjust(10))
+        self.actuatorsWindow.addstr(3, 20, f"{self.actuators_msg.d_fin:.2f} rad".rjust(10))
         self.actuatorsWindow.addstr(4, 4, "p_fin:")
-        self.actuatorsWindow.addstr(4, 17, f"{self.actuators_msg.p_fin:.2f} m".rjust(10))
+        self.actuatorsWindow.addstr(4, 20, f"{self.actuators_msg.p_fin:.2f} rad".rjust(10))
         self.actuatorsWindow.addstr(5, 4, "s_fin:")
-        self.actuatorsWindow.addstr(5, 17, f"{self.actuators_msg.s_fin:.2f} m".rjust(10))
+        self.actuatorsWindow.addstr(5, 20, f"{self.actuators_msg.s_fin:.2f} rad".rjust(10))
         self.actuatorsWindow.refresh()
 
     def battery_window(self):
         self.batteryWindow.addstr(1, 2, "• Battery", curses.A_BOLD | curses.color_pair(3))
+
+        # Timestamp
+        duration = self.duration_ms_from_times(self.battery_card_time)
+        if (duration < 100):
+            color = curses.color_pair(1)
+        else:
+            color = curses.color_pair(2)
+        self.batteryWindow.addstr(1, 20, f"{duration} ms".rjust(10), color)
+
         self.batteryWindow.addstr(2, 4, "tension:")
-        self.batteryWindow.addstr(2, 19, f"{self.battery_card_msg.voltage:.2f} V".rjust(10))
+        self.batteryWindow.addstr(2, 20, f"{self.battery_card_msg.voltage:.2f} V".rjust(10))
         self.batteryWindow.addstr(3, 4, "current:")
-        self.batteryWindow.addstr(3, 19, f"{self.battery_card_msg.current:.2f} A".rjust(10))
+        self.batteryWindow.addstr(3, 20, f"{self.battery_card_msg.current:.2f} A".rjust(10))
         self.batteryWindow.refresh()
 
     def imu_window(self):
         self.imuWindow.addstr(1, 2, "• Imu", curses.A_BOLD | curses.color_pair(3))
+
+        # Timestamp
+        duration = self.duration_ms_from_times(self.imu_time)
+        if (duration < 100):
+            color = curses.color_pair(1)
+        else:
+            color = curses.color_pair(2)
+        self.imuWindow.addstr(1, 18, f"{duration} ms".rjust(10), color)
+
         self.imuWindow.addstr(2, 4, "Linear acceleration:")
         self.imuWindow.addstr(3, 6, "x:")
         self.imuWindow.addstr(3, 20, f"{self.imu_msg.linear_acceleration.x:.2f} m/s^2".rjust(10))
@@ -146,15 +194,19 @@ class RiptideWTF(Node):
 
     def pressure_callback(self, msg):
         self.pressure_msg = msg
+        self.pressure_time = msg.header.stamp
 
     def battery_card_callback(self, msg):
         self.battery_card_msg = msg
+        self.battery_card_time = msg.header.stamp
 
     def actuators_callback(self, msg):
         self.actuators_msg = msg
+        self.actuators_time = msg.header.stamp
 
     def imu_callback(self, msg):
         self.imu_msg = msg
+        self.imu_time = msg.header.stamp
 
 
 def main(args=None):
